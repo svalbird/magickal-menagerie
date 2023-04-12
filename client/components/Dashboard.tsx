1c6e3d607807d183fbab2deeaaa0ca29157b70fe
David Gutierrez <thedoctorguro@gmail.com>
2023-04-12 15:29:48 +1200

    Installs dependencies. Done with love.

---
+++ client/components/Dashboard.tsx
@@ -0,0 +1,1 @@
+// I want to create a dashboard, with a 
--- client/index.tsx
+++ client/index.tsx
@@ -2,12 +2,16 @@ import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
 import store from './store'
 
+import { ChakraProvider } from '@chakra-ui/react'
+
 import App from './components/App'
 
 document.addEventListener('DOMContentLoaded', () => {
   createRoot(document.getElementById('app') as HTMLElement).render(
     <Provider store={store}>
-      <App />
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
     </Provider>
   )
 })
