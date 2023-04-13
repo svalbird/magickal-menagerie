c3d7e3567690a4408c943a6a276b12c2721968c6
David Gutierrez <thedoctorguro@gmail.com>
2023-04-13 17:03:18 +1200

    Creates the PetInteractionDashboard component (empty), updates the Navbar component, adds the petInteraction route to the server.ts file. Creates the petInteractions routes and db functions pages. Adds pixe food library . Done with love.

---
+++ server/db/petInteractions.ts
@@ -0,0 +1,49 @@
+import connection from './connection'
+
+export function getPetInfo(userId: number, db = connection) {
+  return db('pets')
+    .join('user', 'pets.user_id', 'user.id')
+    .join('species', 'pets.species_id', 'species.id')
+    .where('user_id', userId)
+    .select(
+      'pets.id as id',
+      'user_id as userId',
+      'species_id as speciesId',
+      'pets.name as petName',
+      'species.name as speciesName',
+      'xp_current as xpCurrent',
+      'hp_current as hpCurrent',
+      'hunger_current as hungerCurrent',
+      'level'
+    )
+
+    .orderBy('pets.id')
+}
+
+export function getUserInventory(userId: number, db = connection) {
+  return db('inventory')
+    .join('user', 'inventory.user_id', 'user.id')
+    .join('items', 'inventory.item_id', 'items.id')
+    .where('user_id', userId)
+    .select(
+      'inventory.id',
+      'user_id as userId',
+      'item_id as itemId',
+      'name',
+      'type',
+      'description',
+      'hunger_fill as hungerFill',
+      'hp_fill as hpFill',
+      'image'
+    )
+
+    .orderBy('inventory.id')
+}
+export function updatePetInfo(newPet, db = connection) {
+  const pet = newPet // extract the pet object from the newPet parameter
+  return db('pets').where('id', pet.id).update(pet) // update the pet object
+}
+
+export function deleteInventoryItem(id: number, db = connection) {
+  return db('inventory').where('id', id).delete()
+}
--- server/server.ts
+++ server/server.ts
@@ -1,9 +1,11 @@
 import express from 'express'
 import path from 'path'
+import petInteractionRoutes from './routes/petInteractions'
 
 const server = express()
 
 server.use(express.json())
 server.use(express.static(path.join(__dirname, 'public')))
+server.use('/api/v1/petInteractions', petInteractionRoutes)
 
 export default server
