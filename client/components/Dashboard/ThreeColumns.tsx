9bb8ccc846ec0f430f0680010057b716923ec7a8
David Gutierrez <114845085+Om-GG@users.noreply.github.com>
2023-04-17 16:28:50 +1200

    Adds a pet widget to the Dashboard component. Done with love and teamwork

--- client/components/Dashboard/ThreeColumns.tsx
+++ client/components/Dashboard/ThreeColumns.tsx
@@ -1,13 +1,23 @@
-import { ReactElement } from 'react';
-import { Box, SimpleGrid, Icon, Text, Stack, Flex } from '@chakra-ui/react';
-import { FcAssistant, FcDonate, FcInTransit } from 'react-icons/fc';
+import { ReactElement, useEffect } from 'react'
+import {
+  Box,
+  SimpleGrid,
+  Icon,
+  Text,
+  Stack,
+  Flex,
+  Image,
+  Progress,
+} from '@chakra-ui/react'
+import { FcDonate, FcInTransit } from 'react-icons/fc'
+import { useAppDispatch, useAppSelector } from '../../hooks/hooks'
+import { fetchPetData } from '../../actions/petInteractions'
 
 interface FeatureProps {
-  title: string;
-  text: string;
-  icon: ReactElement;
+  title: string
+  text: string
+  icon: ReactElement
 }
-
 
 const Feature = ({ title, text, icon }: FeatureProps) => {
   return (
@@ -20,26 +30,97 @@ const Feature = ({ title, text, icon }: FeatureProps) => {
         color={'white'}
         rounded={'full'}
         bg={'gray.100'}
-        mb={1}>
+        mb={1}
+      >
         {icon}
       </Flex>
       <Text fontWeight={600}>{title}</Text>
       <Text color={'gray.600'}>{text}</Text>
     </Stack>
-  );
-};
+  )
+}
 
 export default function SimpleThreeColumns() {
+  const petInteractions = useAppSelector((state) => state.petInteractions)
+  const dispatch = useAppDispatch()
+  useEffect(() => {
+    dispatch(fetchPetData())
+  }, [dispatch])
   return (
     <Box p={4}>
       <SimpleGrid columns={{ base: 1, md: 3 }} spacing={10}>
-        <Feature
-          icon={<Icon as={FcAssistant} w={10} h={10} />}
-          title={'Lifetime Support'}
-          text={
-            'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore...'
-          }
-        />
+        {petInteractions.data && (
+          <Stack
+            w="100%"
+            h="100%"
+            bg="white"
+            boxShadow="lg"
+            borderRadius="md"
+            p={4}
+            maxW="500px"
+          >
+            <Box w="100%" h="60%">
+              <Image
+                src={petInteractions.data[0].image}
+                alt="gif"
+                w="100%"
+                h="100%"
+                objectFit="cover"
+                borderRadius="md"
+              />
+            </Box>
+            <Box h="40%" mb={4}>
+              <Flex
+                direction="column"
+                alignItems="center"
+                justifyContent="space-around"
+                h="100%"
+              >
+                <Box>
+                  <Text fontWeight={600}>
+                    Level: {petInteractions.data[0].level}
+                  </Text>
+                  <Progress
+                    value={petInteractions.data[0].level}
+                    size="xs"
+                    colorScheme="green"
+                  />
+                </Box>
+                <Box>
+                  <Text color={'gray.600'}>
+                    Exp: {`${petInteractions.data[0].xpCurrent} / 100`}
+                  </Text>
+                  <Progress
+                    value={petInteractions.data[0].xpCurrent}
+                    size="xs"
+                  />
+                </Box>
+                <Box>
+                  <Text fontWeight={600}>
+                    Health:
+                    {`${petInteractions.data[0].hpCurrent} / ${petInteractions.data[0].hpMax}`}
+                  </Text>
+                  <Progress
+                    value={petInteractions.data[0].hpCurrent}
+                    size="xs"
+                    colorScheme="red"
+                  />
+                </Box>
+                <Box>
+                  <Text color={'gray.600'}>
+                    Hunger:
+                    {`${petInteractions.data[0].hungerCurrent} / ${petInteractions.data[0].hungerMax}`}
+                  </Text>
+                  <Progress
+                    value={petInteractions.data[0].hungerCurrent}
+                    size="xs"
+                    colorScheme="orange"
+                  />
+                </Box>
+              </Flex>
+            </Box>
+          </Stack>
+        )}
         <Feature
           icon={<Icon as={FcDonate} w={10} h={10} />}
           title={'Unlimited Donations'}
@@ -56,5 +137,5 @@ export default function SimpleThreeColumns() {
         />
       </SimpleGrid>
     </Box>
-  );
+  )
 }
