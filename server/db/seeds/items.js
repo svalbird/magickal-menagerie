2886f57e3c34265e481c38f2261de0180be8a262
keanu-pere <83375977+Keanu-Pere@users.noreply.github.com>
2023-04-18 16:15:20 +1200

    seeds fixed and ran like the wind

--- server/db/seeds/items.js
+++ server/db/seeds/items.js
@@ -11,7 +11,8 @@ exports.seed = async function (knex) {
       name: 'Beer',
       type: 'food',
       description: 'A nice cold brew.',
-      image: '/Images/FreePixelFood/Assets/FreePixelFood/Sprite/Food/Beer.png',
+      image:
+        '/Images/FreePixelFood/Assets/FreePixelFood/Sprite/Food/Beer.png',
       hunger_fill: 40,
     },
     {
@@ -36,57 +37,68 @@ exports.seed = async function (knex) {
       id: 4,
       name: 'Sausage',
       type: 'Food',
-      description: 'Sausage what more do i need to say',
+      description:
+        'Sausage what more do i need to say',
       image:
-        '/Images/FreePixelFood/Assets/FreePixelFood/Sprite/Food/sausages.png',
+        '/Images/FreePixelFood/Assets/FreePixelFood/Sprite/Food/Sausages.png',
       hunger_fill: 5,
     },
     {
       id: 5,
-      name: 'Wine',
+      name: 'wine',
       type: 'food',
-      description: 'A green yummy wasibi pea like wine.',
-      image: '/Images/FreePixelFood/Assets/FreePixelFood/Sprite/Food/Wine.png',
+      description:
+        'A green yummy wasibi pea like wine.',
+      image:
+        '/Images/FreePixelFood/Assets/FreePixelFood/Sprite/Food/Wine.png',
       hunger_fill: 15,
     },
     {
       id: 6,
-      name: 'Steak',
+      name: 'steak',
       type: 'food',
-      description: 'A yummy piece of cow.',
-      image: '/Images/FreePixelFood/Assets/FreePixelFood/Sprite/Food/steak.png',
+      description:
+        'A yummy piece of cow.',
+      image:
+        '/Images/FreePixelFood/Assets/FreePixelFood/Sprite/Food/Steak.png',
       hunger_fill: 25,
     },
     {
       id: 7,
-      name: 'Pizza',
+      name: 'pizza',
       type: 'food',
-      description: 'A yummy slice of heaven.',
-      image: '/Images/pizza.png',
+      description:
+        'A yummy slice of heaven.',
+      image:
+        '/Images/pizza.png',
       hunger_fill: 15,
     },
     {
       id: 8,
-      name: 'Whiskey',
+      name: 'whiskey',
       type: 'medicine',
-      description: 'A concoction that makes creature feel good.',
+      description:
+        'A concoction that makes creature feel good.',
       image:
-        '/Images/FreePixelFood/Assets/FreePixelFood/Sprite/Food/Avocado.png',
+        '/Images/FreePixelFood/Assets/FreePixelFood/Sprite/Food/Whiskey.png',
       hunger_fill: 25,
     },
     {
       id: 9,
-      name: 'Jerky',
+      name: 'jerky',
       type: 'food',
-      description: 'A dried up piece of meat.',
-      image: '/Images/FreePixelFood/Assets/FreePixelFood/Sprite/Food/Jerky.png',
+      description:
+        'A dried up piece of meat.',
+      image:
+        '/Images/FreePixelFood/Assets/FreePixelFood/Sprite/Food/Jerky.png',
       hunger_fill: 25,
     },
     {
       id: 10,
-      name: 'Avocado',
+      name: 'avocado',
       type: 'food',
-      description: 'A green yummy thing used by many.',
+      description:
+        'A green yummy thing used by many.',
       image:
         '/Images/FreePixelFood/Assets/FreePixelFood/Sprite/Food/Avocado.png',
       hunger_fill: 25,
@@ -107,15 +119,18 @@ exports.seed = async function (knex) {
       type: 'food',
       description:
         'Now this is a Apple which will conter that of the Wormed Apple',
-      image: '/Images/FreePixelFood/Assets/FreePixelFood/Sprite/Food/Apple.png',
+      image:
+        '/Images/FreePixelFood/Assets/FreePixelFood/Sprite/Food/Apple.png',
       hunger_fill: 10,
     },
     {
       id: 13,
       name: 'Bacon',
       type: 'food',
-      description: 'Nice crispy Bacon ready to please!.',
-      image: '/Images/FreePixelFood/Assets/FreePixelFood/Sprite/Food/Bacon.png',
+      description:
+        'Nice crispy Bacon ready to please!.',
+      image:
+        '/Images/FreePixelFood/Assets/FreePixelFood/Sprite/Food/Bacon.png',
       hunger_fill: 25,
     },
     {
@@ -124,14 +139,16 @@ exports.seed = async function (knex) {
       type: 'Poisoned food',
       description:
         'Looks Good to eat but has been Poisoned by the Nasty witch of the south',
-      image: '/Images/FreePixelFood/Assets/FreePixelFood/Sprite/Food/Boar.png',
+      image:
+        '/Images/FreePixelFood/Assets/FreePixelFood/Sprite/Food/Boar.png',
       hunger_fill: -30,
     },
     {
       id: 15,
       name: 'Cookie',
       type: 'food',
-      description: 'The best in the east look no futher its cookie time!.',
+      description:
+        'The best in the east look no futher its cookie time!.',
       image:
         '/Images/FreePixelFood/Assets/FreePixelFood/Sprite/Food/Cookie.png',
       hunger_fill: 25,
@@ -140,44 +157,50 @@ exports.seed = async function (knex) {
       id: 16,
       name: 'MelonWater',
       type: 'drink',
-      description: 'A freshing drink that tastes like watermelon',
+      description:
+        'A freshing drink that tastes like watermelon',
       image:
         '/Images/FreePixelFood/Assets/FreePixelFood/Sprite/Food/MelonWater.png',
       hunger_fill: 20,
     },
     {
       id: 17,
-      name: 'Sardines',
+      name: 'sardines',
       type: 'food',
-      description: 'Stink smelly little fish',
+      description:
+        'Stink smelly little fish',
       image:
         '/Images/FreePixelFood/Assets/FreePixelFood/Sprite/Food/Sardines.png',
       hunger_fill: -25,
     },
     {
       id: 18,
-      name: 'Bug',
+      name: 'bug',
       type: 'misc',
-      description: 'Dont eat me I am gross',
-      image: '/Images/FreePixelFood/Assets/FreePixelFood/Sprite/Food/Bug.png',
+      description:
+        'Dont eat me I am gross',
+      image:
+        '/Images/FreePixelFood/Assets/FreePixelFood/Sprite/Food/Bug.png',
       hunger_fill: -25,
     },
     {
       id: 19,
-      name: 'Dragonfruit',
+      name: 'dragonfruit',
       type: 'food',
-      description: 'The blood of my enemies',
+      description:
+        'The blood of my enemies',
       image:
         '/Images/FreePixelFood/Assets/FreePixelFood/Sprite/Food/DragonFruit.png',
       hunger_fill: 80,
     },
     {
       id: 20,
-      name: 'Cherry',
+      name: 'olive',
       type: 'food',
-      description: 'A delicious piece of fruit',
+      description:
+        'A delicious piece of fruit',
       image:
-        '/Images/FreePixelFood/Assets/FreePixelFood/Sprite/Food/Cherry.png',
+        '/Images/FreePixelFood/Assets/FreePixelFood/Sprite/Food/Olive.png',
       hunger_fill: 10,
     },
     {
@@ -197,7 +220,7 @@ exports.seed = async function (knex) {
       description:
         'A warm and sweet-smelling bun, freshly baked from Honeybun Bakery in Bellston',
       image:
-        '/Images/FreePixelFood/Assets/FreePixelFood/Sprite/Food/Cherry.png',
+        '/Images/FreePixelFood/Assets/FreePixelFood/Sprite/Food/Honeycomb.png',
       hunger_fill: 40,
     },
   ])
